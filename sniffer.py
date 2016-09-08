55fc169d09cb55e9786f302aa43619e154d12aa2
Charles MacDuff <Charles_5234@hotmail.com>
2016-09-08 11:21:50 -0400

    commit tp1a
    commit tp1a

--- sniffer.py
+++ sniffer.py
@@ -7,6 +7,7 @@ def dnsMonitorCallBack(pkt):
         send(packet)
 
 def NewPacket(pkt):
+    global idDNS
     packet = IP(src=pkt[IP].dst, dst=pkt[IP].src) / \
              UDP(sport=pkt[UDP].dport, dport=pkt[UDP].sport) / \
              DNS(id=pkt[DNS].id,qr=1,rd=1,ra=1, qd=DNSQR(qname=pkt[DNS][DNSQR].qname,qtype=pkt[DNS][DNSQR].qtype,qclass=pkt[DNS][DNSQR].qclass), an=DNSRR(rrname=pkt[DNS][DNSQR].qname, type="A", rclass="IN", ttl=267, rdata="172.16.1.66"))
